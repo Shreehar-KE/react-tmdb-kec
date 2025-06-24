f638247c4593f749157733dac8802369a3fd1ed3
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-25 05:27:44 +0530

    add until fetch API setup

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(`Error fetching movie: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="hero.png" alt="" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
+      </div>
+    </main>
+  );
+}
+
+export default App;
