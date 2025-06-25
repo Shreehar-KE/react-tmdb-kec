b97a830c03b5cdf35bbd44fdf9ec72f194a21b0e
Shreehar-KE <shreehar.elancheran@gmail.com>
2025-06-25 13:16:36 +0530

    add Spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -68,7 +69,7 @@ function App() {
           <h2>All Movies</h2>
             {
               isLoading ? 
-                ("Loading...")
+                <Spinner/>
               : 
                 (
                   <ul>
